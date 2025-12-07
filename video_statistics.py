fced00515937c2693d2394c7d2ee89c7a96aec0b
panda <mengzhiyuqing@gmail.com>
2025-12-08 10:34:29 +1100

    added extract_video_data function

--- video_statistics.py
+++ video_statistics.py
@@ -47,10 +47,40 @@ def get_video_ids(playlistId):
     except requests.exceptions.RequestException as e:
         print(f"An error occurred: {e}")   
 
-
-# 如果直接运行这个脚本，才执行函数 
-    
+def extract_video_data(video_ids):
+    extracted_data=[]
+    def batch_video_ids(video_id_list,batch_size):
+        for i in range(0,len(video_id_list),batch_size):
+            yield video_id_list[i:i+batch_size]
+    try:
+        for ids in batch_video_ids(video_ids,maxResults):
+            video_ids_str=",".join(ids)
+            url=f"https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails,snippet,statistics&id={video_ids_str}&key={API_KEY}"
+            response=requests.get(url)
+            response.raise_for_status()
+            data=response.json()
+            
+            for i in data.get("items",[]):
+                video_id=i['id']
+                snippet=i['snippet']
+                contentDetails=i['contentDetails']
+                statistics=i['statistics']
+                
+                video_data={
+                    "video_id":video_id,
+                    "title":snippet.get("title"),
+                    "publishedAt":snippet.get("publishedAt"),
+                    "duration":contentDetails.get("duration"),
+                    "viewCount":statistics.get("viewCount",None),
+                    "likeCount":statistics.get("likeCount",None),
+                    "commentCount":statistics.get("commentCount",None),
+                }
+                extracted_data.append(video_data)
+        return extracted_data
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred:{e}")    
 if __name__ == "__main__":   
     playlistId=get_playlist_id()
-    print(get_video_ids(playlistId))
-    
+    video_ids=get_video_ids(playlistId)
+    extract_video_data(video_ids)
+        
